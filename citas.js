9d98ab353cad3e8cf02c1654b0a264bafa1af058
LuisArcila <lesantoar@cesde.net>
2024-09-03 18:06:31 -0500

    se crean cambios

---
+++ citas.js
@@ -0,0 +1,83 @@
+let citas =[
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : '. La mejor venganza es ser diferente a quien causó el daño.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : '. Lo que no es útil para la colmena, no es útil para la abeja.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Si no conviene, no lo hagas; si no es verdad, no lo digas. Sé dueño de tus inclinaciones'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Realiza cada una de tus acciones como si fuera la última de tu vida.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' No obres como si fueras a vivir mil años; obra como si el fin estuviera muy cerca.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' La dulzura, cuando es sincera, es una fuerza invencible.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' El arte de vivir se asemeja más a la lucha que a la danza.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : 'El mundo no es más que transformación, y la vida, opinión solamente.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Tranquilo a la vez que resuelto, alegre a la par que consistente, es el hombre que en todo sigue la razón.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' En ninguna parte puede hallar el hombre un retiro tan imperturbable y tranquilo como en la intimidad de su alma.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' El tiempo es como un río que arrastra rápidamente todo lo que nace.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Una sola es la luz del sol, aunque la obstaculicen muros, montes, incontables impedimentos.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' No malgastes más tiempo argumentando acerca de lo que debe ser un buen hombre. Trata de ser uno.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' No permitas que tu memoria se enajene de las cosas que tienes, sino de las que te hagan falta.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Tienes poder sobre tu mente, no sobre los acontecimientos. Date cuenta de esto y encontrarás la fuerza.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Todas las cosas se hallan entrelazadas entre sí y su común vínculo es sagrado y casi ninguna es extraña a la otra, porque todas están coordinadas y contribuyen al orden del mismo mundo.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Cuánto tiempo ahorra el que no se da la vuelta para ver lo que su vecino hace, dice o piensa.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Hemos nacido para colaborar, al igual que los pies, las manos, los párpados, las hileras de dientes, superiores e inferiores. Obrar, pues, como adversarios los unos de los otros es contrario a la naturaleza.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Todo lo que escuchamos es una opinión, no un hecho. Todo lo que vemos es una perspectiva, no la verdad.'
+    },
+    {
+        'autor' : 'Marco Aurelio',
+        'texto' : ' Comenzar es la mitad del trabajo, comienza nuevamente con la mitad restante, y habrás terminado.'
+    }
+   
+]
